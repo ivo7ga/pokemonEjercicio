f4425b2a6cca13786605bf46e57e1ac8407426b4
ivo7ga <ivan_gonzalez_tsapp1oa2021@cev.com>
2022-06-16 14:20:56 +0200

    Initial Commit

---
+++ pokemonEjercicio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pokemonEjercicio
+//
+//  Created by Iván González on 16/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
